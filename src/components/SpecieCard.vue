feb072e54428a51834fc5839296b56a87262bfba
JhinAhri12 <cyprien.rezki@hotmail.fr>
2022-09-20 11:35:39 +0200

    complete specieCard component

--- src/components/SpecieCard.vue
+++ src/components/SpecieCard.vue
@@ -1,4 +1,22 @@
 <template>
-  Panda
-  <img src="" alt="panda">
+  <router-link
+    class="specie-link"
+    :to="{ name: 'SpecieDetails', params: { id: specie.id } }"
+  >
+    <div class="specie-card">
+      <span>@{{ specie.time }} on {{ specie.date }}</span>
+      <h4>{{ specie.title }}</h4>
+    </div>
+  </router-link>
 </template>
+
+<script>
+export default {
+  props: {
+    specie: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,15 @@
-<script setup>
-import Specie from '../components/SpecieCard.vue'
-</script>
-
 <template>
   <h1>List of endangered species</h1>
 </template>
+
+<script>
+import EventService from "@/services/EventService.js";
+export default {
+  name: "HomeView",
+  data(){
+    return {
+      species: null
+    }
+  }
+}
+</script>
